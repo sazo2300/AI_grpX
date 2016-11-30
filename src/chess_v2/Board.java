504e8e3883053d46899b96c6e0580cd3e5ec128c
FlazH <sazo-2@hotmail.com>
2016-12-01 00:35:45 +0100

    field

--- src/chess_v2/Board.java
+++ src/chess_v2/Board.java
@@ -17,6 +17,35 @@ public class Board {
     private List<Move> validBlackMoves;
     
     public Board() {
+        for(int i = 0 ; i < 8 ; i++){
+            state[1][i] = new Field("a2", new Piece(Game.Color.WHITE, Piece.Type.PAWN));
+            state[6][i] = new Field("a2", new Piece(Game.Color.BLACK, Piece.Type.PAWN));
+        }
+        for(int i = 2 ; i < 6 ; i++){
+            for(int j = 0 ; j < 8 ; j++){
+                state[i][j] = new Field("a2", new Piece(Game.Color.WHITE, Piece.Type.BLANK));
+            }
+            
+        }
+ 
+        state[0][0] = new Field("a1", new Piece(Game.Color.WHITE, Piece.Type.ROOK));
+        state[0][1] = new Field("b1", new Piece(Game.Color.WHITE, Piece.Type.KNIGHT));
+        state[0][2] = new Field("c1", new Piece(Game.Color.WHITE, Piece.Type.BISHOP));
+        state[0][3] = new Field("d1", new Piece(Game.Color.WHITE, Piece.Type.QUEEN));
+        state[0][4] = new Field("e1", new Piece(Game.Color.WHITE, Piece.Type.KING));
+        state[0][5] = new Field("f1", new Piece(Game.Color.WHITE, Piece.Type.BISHOP));
+        state[0][6] = new Field("g1", new Piece(Game.Color.WHITE, Piece.Type.KNIGHT));
+        state[0][7] = new Field("h1", new Piece(Game.Color.WHITE, Piece.Type.ROOK));
+        
+        state[7][0] = new Field("a1", new Piece(Game.Color.BLACK, Piece.Type.ROOK));
+        state[7][1] = new Field("b1", new Piece(Game.Color.BLACK, Piece.Type.KNIGHT));
+        state[7][2] = new Field("c1", new Piece(Game.Color.BLACK, Piece.Type.BISHOP));
+        state[7][3] = new Field("d1", new Piece(Game.Color.BLACK, Piece.Type.QUEEN));
+        state[7][4] = new Field("e1", new Piece(Game.Color.BLACK, Piece.Type.KING));
+        state[7][5] = new Field("f1", new Piece(Game.Color.BLACK, Piece.Type.BISHOP));
+        state[7][6] = new Field("g1", new Piece(Game.Color.BLACK, Piece.Type.KNIGHT));
+        state[7][7] = new Field("h1", new Piece(Game.Color.BLACK, Piece.Type.ROOK));
+        
         
     }
     
@@ -30,6 +59,20 @@ public class Board {
     
     public void move(Move move) {
         
+    }
+    
+    public void printBoard(){
+        StringBuilder board = new StringBuilder();
+        for(int i = 7 ; i >= 0 ; i--){
+            board.append("\n" + (i + 1));
+            for(int j = 7 ; j >= 0 ; j--){
+                if(state[i][j].getPiece().getType() != Piece.Type.BLANK)
+                board.append(state[i][j].getPiece().getSymbol() + "|");
+                else
+                    board.append(" |");
+            } 
+        }
+        board.append("   a   b   c   d   e   f   g   h");
     }
     
     
--- src/chess_v2/Piece.java
+++ src/chess_v2/Piece.java
@@ -7,11 +7,12 @@ import java.util.List;
  * @author atila29
  */
 public class Piece {
-    public enum Type {PAWN, ROOK, KNIGHT, BISHOP, QUEEN, KING}
+    public enum Type {BLANK, PAWN, ROOK, KNIGHT, BISHOP, QUEEN, KING}
     private Game.Color color;
     private Type type;
     private int value;
     private List<Move> moves;
+    private String symbol;
 
     public Piece(Game.Color color, Type type) {
         this.type = type;
@@ -19,16 +20,40 @@ public class Piece {
         
         if(type == Type.PAWN) {
             this.value = 2;
+            if(color.equals(Game.Color.WHITE))
+                symbol = new String(Character.toChars(9817));
+             else 
+                symbol = new String(Character.toChars(9823));
         } else if(type == Type.BISHOP) {
             this.value = 5;
+            if(color.equals(Game.Color.WHITE))
+                symbol = new String(Character.toChars(9815));
+             else 
+                symbol = new String(Character.toChars(9821));
         } else if(type == Type.KNIGHT) {
             this.value = 7;
+            if(color.equals(Game.Color.WHITE))
+                symbol = new String(Character.toChars(9816));
+             else 
+                symbol = new String(Character.toChars(9822));
         } else if(type == Type.ROOK) {
             this.value = 4;
+            if(color.equals(Game.Color.WHITE))
+                symbol = new String(Character.toChars(9814));
+             else 
+                symbol = new String(Character.toChars(9820));
         } else if(type == Type.QUEEN) {
             this.value = 20;
+            if(color.equals(Game.Color.WHITE))
+                symbol = new String(Character.toChars(9813));
+             else 
+                symbol = new String(Character.toChars(9819));
         } else if(type == Type.KING) {
             this.value = Integer.MAX_VALUE;
+            if(color.equals(Game.Color.WHITE))
+                symbol = new String(Character.toChars(9812));
+             else 
+                symbol = new String(Character.toChars(9818));
         }
     }
     
@@ -62,5 +87,9 @@ public class Piece {
 
     public void setMoves(List<Move> moves) {
         this.moves = moves;
+    }
+    
+    public String getSymbol(){
+        return symbol;
     }
 }
