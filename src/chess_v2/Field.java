d27f80e6d64fbbf258dae582794fb9598808b947
FlazH <sazo-2@hotmail.com>
2016-11-30 23:36:01 +0100

    field

--- src/chess_v2/Field.java
+++ src/chess_v2/Field.java
@@ -12,11 +12,101 @@ public class Field {
     private String boardIndex;
     
     public Field(String f) {
-        // omskriv fra board til position.
+        int indexY;
+        int indexX;
+        if(f.charAt(0) == 'a'){
+            indexY = 0;
+            String temp = f.replaceAll("\\D+","");
+            indexX = Integer.valueOf(f) - 1;
+            position = new Position(indexX, indexY);
+        }
+        else if(f.charAt(0) == 'b'){
+            indexY = 1;
+            String temp = f.replaceAll("\\D+","");
+            indexX = Integer.valueOf(f) - 1;
+            position = new Position(indexX, indexY);
+        }
+        else if(f.charAt(0) == 'c'){
+            indexY = 2;
+            String temp = f.replaceAll("\\D+","");
+            indexX = Integer.valueOf(f) - 1;
+            position = new Position(indexX, indexY);
+        }
+        else if(f.charAt(0) == 'd'){
+            indexY = 3;
+            String temp = f.replaceAll("\\D+","");
+            indexX = Integer.valueOf(f) - 1;
+            position = new Position(indexX, indexY);
+        }
+        else if(f.charAt(0) == 'e'){
+            indexY = 4;
+            String temp = f.replaceAll("\\D+","");
+            indexX = Integer.valueOf(f) - 1;
+            position = new Position(indexX, indexY);
+        }
+        else if(f.charAt(0) == 'f'){
+            indexY = 5;
+            String temp = f.replaceAll("\\D+","");
+            indexX = Integer.valueOf(f) - 1;
+            position = new Position(indexX, indexY);
+        }
+        else if(f.charAt(0) == 'g'){
+            indexY = 6;
+            String temp = f.replaceAll("\\D+","");
+            indexX = Integer.valueOf(f) - 1;
+            position = new Position(indexX, indexY);
+        }
+        else if(f.charAt(0) == 'h'){
+            indexY = 7;
+            String temp = f.replaceAll("\\D+","");
+            indexX = Integer.valueOf(f) - 1;
+            position = new Position(indexX, indexY);
+        }
     }
     
     public Field(int x, int y){
-        position = new Position(x, y);
+        String indexY;
+        String indexX;
+        if(y == 0){
+            indexY = "a";
+            indexX = String.valueOf(x);
+            boardIndex = indexY+indexX;
+        }
+        else if(y == 1){
+            indexY = "b";
+            indexX = String.valueOf(x);
+            boardIndex = indexY+indexX;
+        }
+        else if(y == 2){
+            indexY = "c";
+            indexX = String.valueOf(x);
+            boardIndex = indexY+indexX;
+        }
+        else if(y == 3){
+            indexY = "d";
+            indexX = String.valueOf(x);
+            boardIndex = indexY+indexX;
+        }
+        else if(y == 4){
+            indexY = "e";
+            indexX = String.valueOf(x);
+            boardIndex = indexY+indexX;
+        }
+        else if(y == 5){
+            indexY = "f";
+            indexX = String.valueOf(x);
+            boardIndex = indexY+indexX;
+        }
+        else if(y == 6){
+            indexY = "g";
+            indexX = String.valueOf(x);
+            boardIndex = indexY+indexX;
+        }
+        else if(y == 7){
+            indexY = "h";
+            indexX = String.valueOf(x);
+            boardIndex = indexY+indexX;
+        }
     }
     
     public class Position {        
