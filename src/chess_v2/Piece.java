89b54492d8f0e85b693451013508cab0a4fc3fd5
atila29 <mathias.petersen95@gmail.com>
2016-11-30 23:27:11 +0100

    fix kinda we're fuck'd

--- src/chess_v2/Piece.java
+++ src/chess_v2/Piece.java
@@ -1,9 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package chess_v2;
+
+import java.util.List;
 
 /**
  *
@@ -11,5 +8,59 @@ package chess_v2;
  */
 public class Piece {
     public enum Type {PAWN, ROOK, KNIGHT, BISHOP, QUEEN, KING}
+    private Game.Color color;
+    private Type type;
+    private int value;
+    private List<Move> moves;
+
+    public Piece(Game.Color color, Type type) {
+        this.type = type;
+        this.color = color;
+        
+        if(type == Type.PAWN) {
+            this.value = 2;
+        } else if(type == Type.BISHOP) {
+            this.value = 5;
+        } else if(type == Type.KNIGHT) {
+            this.value = 7;
+        } else if(type == Type.ROOK) {
+            this.value = 4;
+        } else if(type == Type.QUEEN) {
+            this.value = 20;
+        } else if(type == Type.KING) {
+            this.value = Integer.MAX_VALUE;
+        }
+    }
     
+    public Game.Color getColor() {
+        return color;
+    }
+
+    public void setColor(Game.Color color) {
+        this.color = color;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public List<Move> getMoves() {
+        return moves;
+    }
+
+    public void setMoves(List<Move> moves) {
+        this.moves = moves;
+    }
 }
